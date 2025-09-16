e624736cb6cecaca807a7d5a77b38be3524064c7
Alejandro Bogotá <alejandrobogota@Alejandros-MacBook-Air.local>
2025-09-16 13:46:23 -0500

    Initial commit

---
+++ Micro-1/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify(status="ok"), 200
+
+@app.route("/example", methods=["GET"])
+def example():
+    return jsonify(message="ejemplo de servicio en Flask"), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
